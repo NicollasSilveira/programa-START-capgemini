f93e49eb27a9874f1897df688c627b61cbc9fb53
NicollasSilveira <nicollasprog@gmail.com>
2025-08-25 19:58:17 -0300

    Variaveis e Operadores

---
+++ VariaveisEOperadores/src/main/java/_VariaveisEOperadores/Main.java
@@ -0,0 +1,26 @@
+package _VariaveisEOperadores;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int idade; // Numeros Inteiros
+		float pi = 3.14f; // Numero de Ponto Flutuante
+		double precisao = 0.0000000000000000000017d; // Como o o tipo float porem mais preiso
+		char genero = 'M'; // Armazena apenas 1 digito
+		byte  placar = 5; // Armazena numeros entre -127 e 127
+		boolean isAtivo = true; // Ou false
+		
+		String nome = "Nicollas Silveira"; // Aramazena texto
+		
+		/*
+		 *  + Adição / Concatenar
+		 *  - Subtração
+		 *  / Divisão
+		 *  * Multiplicação
+		 *  % Resto
+		 */
+
+	}
+
+}
