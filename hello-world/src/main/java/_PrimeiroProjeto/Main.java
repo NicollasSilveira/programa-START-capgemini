b858936a6326e7e9daaa9eb2bdc37ad79b297540
NicollasSilveira <nicollasprog@gmail.com>
2025-08-25 16:26:22 -0300

    hello world

---
+++ hello-world/src/main/java/_PrimeiroProjeto/Main.java
@@ -0,0 +1,19 @@
+package _PrimeiroProjeto;
+
+public class Main {
+	public static void main(String[] args) {
+		// Comentario de linha
+		
+		/*
+			 Bloco
+			 de
+			 Comentario
+		 */
+		
+		/**
+			Comentario de documentação
+		*/
+		
+		System.out.println("Olá!");
+	}
+}
